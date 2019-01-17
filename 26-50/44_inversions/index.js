b0d02f173dafaf735f2d2a95702afd49e939d482
morris-ribs <maumribeiro@gmail.com>
2019-01-17 15:05:27 +0100

    Problem 44 fix

--- 26-50/44_inversions/index.js
+++ 26-50/44_inversions/index.js
@@ -1,23 +1,47 @@
+class TreeNode {
+  constructor(val) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+    this.counter = 0;
+  }
+
+  insert(val) {
+    if (val < this.val && this.left) {
+      return this.left.insert(val);
+    } else if (val < this.val) {
+      this.left = new TreeNode(val);
+      this.left.counter = this.counter + 1;
+      return this.left.counter;
+    } 
+
+    if (val > this.val && this.right) {
+      return this.right.insert(val);
+    } else {
+      const counter = this.counter++;
+      this.right = new TreeNode(val);
+      return counter;
+    }
+  }
+}
+
+function insertOnTree() {
+
+}
+
 function outOfOrder(arr) {
-  if (arr.length === 0) {
+  if (arr.length <= 1) {
     return 0;
   }
 
   let count = 0;
-  let min = 0, max = 0;
+
+  // insert tree root
+  const root = new TreeNode(arr[0]);
 
   for (let i = 1; i < arr.length; i++) {
     const num = arr[i];
-    if (num > arr[max]) {
-      max = i;
-    } else {
-      if (num < arr[min]) {        
-        min = i;
-        count += (min-0);
-      } else {
-        count++;
-      }
-    }
+    count += root.insert(num);
   }
 
   return count;
@@ -25,3 +49,5 @@ function outOfOrder(arr) {
 
 console.log(outOfOrder([2, 4, 1, 3, 5])); // expected 3
 console.log(outOfOrder([5, 4, 3, 2, 1])); // expected 10
+console.log(outOfOrder([4, 3, 1, 2, 5])); // expected 5
+console.log(outOfOrder([3, 4, 1, 2, 5])); // expected 4
